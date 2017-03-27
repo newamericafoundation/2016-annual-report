48aa2a9c6ad1c7adcd2ca85b0ba62c7a0677cc24
Ronesha D. Dennis <ronesha_dennis@yahoo.com>
2017-03-27 14:19:02 -0400

    changed media query for height

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ $(document).ready(function(){
     };
 
 
-$(window).on("resizeend", function(){
+$(window).on("resizeend", 1000, function(){
       screenType = check_window_size();
       
       if (screenType == "web"){
